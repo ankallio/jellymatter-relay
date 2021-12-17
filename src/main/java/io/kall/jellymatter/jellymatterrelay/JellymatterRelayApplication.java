805be615ca8290e960e8d08dc0d24234429fed32
Antti Kallio <ankallio@users.noreply.github.com>
2021-12-18 01:32:06 +0200

    Imported starter project

---
+++ src/main/java/io/kall/jellymatter/jellymatterrelay/JellymatterRelayApplication.java
@@ -0,0 +1,13 @@
+package io.kall.jellymatter.jellymatterrelay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JellymatterRelayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JellymatterRelayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/kall/jellymatter/jellymatterrelay/JellymatterRelayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kall.jellymatter.jellymatterrelay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JellymatterRelayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
